2ad7efaf334955bc45d1fd61a8875adfc8709af9
JoelLucas <joelcba57@gmail.com>
2025-03-20 16:06:24 +0000

    hack prevent ant rolbak

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -117,6 +117,11 @@ RECOVERY_SDCARD_ON_DATA := true
 # Use mke2fs to create ext4 images
 TARGET_USES_MKE2FS := true
 
+# Hack: prevent anti rollback
+PLATFORM_SECURITY_PATCH := 2099-12-31
+VENDOR_SECURITY_PATCH := 2099-12-31
+PLATFORM_VERSION := 99
+
 # TWRP specific build flags
 TW_THEME := portrait_hdpi
 TW_SCREEN_BLANK_ON_BOOT := true
